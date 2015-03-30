50c32f19c9925819fa491582e255d4926fc26593
SoumyaParida <soumya.parida3@gmail.com>
2015-03-30 11:50:40 +0200

    modified only for website urls

--- alexaCrawl/spiders/alexawebcrawltest.py
+++ alexaCrawl/spiders/alexawebcrawltest.py
@@ -33,59 +33,41 @@ class alexaSpider(CrawlSpider):
     allowed_domains = ["alexa.com"]
     start_urls = [
         "http://www.alexa.com/",
-        "http://www.alexa.com/topsites",
-        #"http://www.alexa.com"    
+        "http://www.alexa.com/topsites", 
     ]
     rules = [
         Rule(sle(allow=("/topsites;?[0-9]*/")), callback='parse_category_top', follow=True),
-        #Rule(sle(allow=("/topsites;2/")), callback='parse_category_top_xxx', follow=True),
     ]
-    
-    items = []
-
     #function to retrieve links from alexa 
-
     def parse_category_top(self, response):
+        global items
         items = []
         sel = Selector(response)
 
         sites = sel.css('.site-listing')
         for site in sites:
             item = alexaSiteInfoItem()
-            item['url'] = site.css('a[href*=siteinfo]::attr(href)')[0].extract()
+            #item['url'] = site.css('a[href*=siteinfo]::attr(href)')[0].extract()
             item['name'] = site.css('a[href*=siteinfo]::text')[0].extract()
-            item['description'] = site.css('.description::text')[0].extract()
+            #item['description'] = site.css('.description::text')[0].extract()
             remainder = site.css('.remainder::text')
-            if remainder:
-                item['description'] += remainder[0].extract()
+            #if remainder:
+             #   item['description'] += remainder[0].extract()
             # more specific
-            item['category'] = response.url.split('/')[-1]
+            #item['category'] = response.url.split('/')[-1]
             items.append(item)
-
             value=item['name']
-            print "value=",value
             if ((value.find("http://") == -1) or (value.find("https://") == -1)):
                 value="http://"+ value
-                print "Send Url with dont filter option************************************",value   
-            
-            print "Sending request",value
+            print "sending value"
             request=Request(url=value,callback=self.parse_details,dont_filter=True)
-            print "Sending requesttttttttttttttttttttttt",request
+            print "receiving value"
             request.meta['item'] = item
         return request
-        #return items   
 
     def parse_details(self,response):
-        #self.log("Visited %s" % response.url)
-        print "response=",response.url
-        print "Start Crawling*****************************************************"
-        #response.replace="http://wikipedia.com"
-        print response.url
+        print("start crawling........................................................")
         allowedDomain=[]
-        global items
-        items=[]
-        #global items
-        #items = []
         if (response.url).find("http://www") == 0:
             allowedDomain=response.url.split("http://www.")
         elif (response.url).find("https://www") == 0:
@@ -95,83 +77,20 @@ class alexaSpider(CrawlSpider):
         elif (response.url).find("https://") == 0:
             allowedDomain=response.url.split("https://")
         
-    #     print "out of index",allowedDomain[1]
         allowed_domains = [allowedDomain[1]]
-    #     print "allowed_domains=",allowed_domains
         start_urls = [response.url]
-        print "start_urls=",start_urls 
-
         hxs = HtmlXPathSelector(response)
-        #hxs = Selector(response)
-        #titles = hxs.select("//span[@class='pl']")
         titles = hxs.select("//ul/li")
-        #titles = hxs.css('.site-listing')
-        print "titles=",titles
-    #     #sel = Selector(response)
-    #     #sites = sel.css('.site-listing')
-        
-        # print "sites=",titles
-        # print "soumyaaaaaaaaaaaaaaaaaaaaaa"
-        # for site in titles:
-        #     item = alexaSiteInfoItem()
-        #     item= site.css('a[href*=siteinfo]::text')[0].extract()
-        #     remainder = site.css('.remainder::text')
-        #     items.append(item)
-        #     print "items=",items
-
+       
         for titles in titles:
             item = alexaSiteInfoItem()
-            #item ["title"] = titles.select("a/text()").extract()
-            item['name'] = titles.select("a/@href").extract() 
+            value=titles.select("a/@href").extract()
+            url = ''.join(value)
+            print "url=========",url
+            if url.find ("http://") ==0 or url.find ("https://")==0 or url.find ("www") ==0:
+                item['name'] =  url
             print "item==========",item
+            print "items***********************************",items
             items.append(item)
 
         return items
-            #items.append(item)
-            #print "items in 2nd iterator=",items
-
-    #     return item
-        # for url in items:
-        #     for value in url:
-        #         print "url=",value
-        # #check for webistes crawled from alexa contain http or https.Else it will show schema
-        # #error when send it using Request.
-        # #some websites already having http or https included while crawling from alexa
-        #         if ((value.find("http://") == -1) or (value.find("https://") == -1)):
-        #            sendUrl="http://"+ url
-        #         print "Send Url with dont filter option*****************************************************",sendUrl
-        #         yield Request(url= sendUrl, callback=self.parse_details,dont_filter=True)
-        #     #items.append(item)
-
-        
-    # def parse_category_top(self, response):
-    #     global items
-    #     items = []
-    #     sel = Selector(response)
-
-    #     sites = sel.css('.site-listing')
-    #     for site in sites:
-    #         item = alexaSiteInfoItem()
-    #         item= site.css('a[href*=siteinfo]::text')[0].extract()
-    #         remainder = site.css('.remainder::text')
-    #         items.append(item)
-    #     return items
-        #for url in items:
-        # #check for webistes crawled from alexa contain http or https.Else it will show schema
-        # #error when send it using Request.
-        # #some websites already having http or https included while crawling from alexa
-        #     if ((url.find("http://") == -1) or (url.find("https://") == -1)):
-        #        sendUrl="http://"+ url
-        #     print "Send Url with dont filter option*****************************************************",sendUrl
-        #     Request(url= sendUrl, callback=self.parse_details,dont_filter=True)
-        #     items.append(item)
-
-        # return items
-
-    
-            
-        #for count in itemsNew:
-         #   print "count=",count
-          #  if ((count != "http://") or (itemsNew[count] != "http://")):
-           #     itemsNew.remove(itemsNew[count])
-        #return itemsNew
